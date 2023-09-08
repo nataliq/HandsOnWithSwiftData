af331dddaf8df3d0d70d3c5df97c101683805f58
Nataliya Patsovska <nataliya.patsovska@gmail.com>
2023-09-08 23:44:50 +0200

    Task 5: Make sure the unit tests compile and comment on the result

--- Finished project/HandsOnWithSwiftDataTests/HandsOnWithSwiftDataTests.swift
+++ Finished project/HandsOnWithSwiftDataTests/HandsOnWithSwiftDataTests.swift
@@ -3,15 +3,30 @@ import XCTest
 import SwiftData
 
 final class HandsOnWithSwiftDataTests: XCTestCase {
-
+    
     @MainActor
     func testModelCreation() throws {
+        let container = try ModelContainer(
+            for: Thought.self, Tag.self,
+            configurations: .init(isStoredInMemoryOnly: true)
+        )
+        
+        let context = container.mainContext
+        
         let tagNames = (1...10).map(String.init)
         let tags = tagNames.map {
-            Tag(name: $0)
+            let tag = Tag(name: $0)
+            context.insert(tag)
+            return tag
         }
-
-        let thought = Thought(text: "test", tags: tags)
+        
+        let thought = Thought(text: "test")
+        thought.tags = tags
+        
+        // This fails - even thought the `tags` relationship is
+        // represented by the `Array` type, its order cannot be
+        // guaranteed to be the same as when the objects were created.
+        // To preserve the order we nee to introduce an attribute to sort by.
         XCTAssertEqual(thought.tags.map { $0.name }, tagNames)
     }
 }
